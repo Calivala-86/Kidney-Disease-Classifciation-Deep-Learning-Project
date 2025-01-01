d7c5209e5037ccd72a9bc49a8bf1526accf863f2
kali986-prog <71525229+kali986-prog@users.noreply.github.com>
2025-01-02 00:56:28 +0100

    download data updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Kidney-Disease-Classifciation-Deep-Learning-Project"
-AUTHOR_USER_NAME = "Calivala-86"
+AUTHOR_USER_NAME = "Fernando Ecumbi"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "fernandoecumbi@hotmail.com"
 
