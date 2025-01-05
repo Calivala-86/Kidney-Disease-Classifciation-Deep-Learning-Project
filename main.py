ff0716ea82fdebf5c3cce83451c5baaa2f5eda11
kali986-prog <71525229+kali986-prog@users.noreply.github.com>
2025-01-05 15:01:04 +0100

    model training added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_02_prepare_base_model import (
     PrepareBaseModelTrainingPipeline,
 )
-
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 if __name__ == "__main__":
@@ -25,6 +25,18 @@ if __name__ == "__main__":
         logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
         obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+
+
+STAGE_NAME = "Training"
+if __name__ == "__main__":
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+        model_trainer = ModelTrainingPipeline()
+        model_trainer.main()
         logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
