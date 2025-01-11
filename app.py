e3cfcaa52cc0decbed8344d2c9d0806813c2e3fd
kali986-prog <71525229+kali986-prog@users.noreply.github.com>
2025-01-11 22:08:19 +0100

    app.py updated

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
-from cnnClassifier.pipeline.prediction import PredictionPipeline
+from cnnClassifier.pipeline.predict import PredictionPipeline
 
 
 os.putenv("LANG", "en_US.UTF-8")
@@ -28,7 +28,7 @@ def home():
 @app.route("/train", methods=["GET", "POST"])
 @cross_origin()
 def trainRoute():
-    os.system("dvc repro")
+    os.system("python main.py")
     return "Training done successfully!"
 
 
--- src/cnnClassifier/pipeline/predict.py
+++ src/cnnClassifier/pipeline/predict.py
@@ -11,13 +11,13 @@ class PredictionPipeline:
         self.filename = filename
 
     def predict(self):
-        model = load_model(os.path.join("artitacts", "training", "model.h5"))
+        model = load_model(os.path.join("artifacts", "training", "model.h5"))
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
-        result = np.argmax(model.predict(test_image), axis=1)
+        result = np.argmax(model.predict(test_image), exis=1)
         print(result)
 
         if result[0] == 1:
