72dba4854db30a53ebd607754adf013b455ed316
kali986-prog <71525229+kali986-prog@users.noreply.github.com>
2025-01-11 21:29:22 +0100

    app.py added

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing import image
+from tensorflow.python.keras.models import load_model
+import os
+
+
+class PredictionPipeline:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def predict(self):
+        model = load_model(os.path.join("artitacts", "training", "model.h5"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size=(224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = "Tumor"
+            return [{"image": prediction}]
+        else:
+            prediction = "Normal"
+            return [{"image": prediction}]
